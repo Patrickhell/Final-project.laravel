84cad6455dc19e39b55452a59f5ab12db5669209
Patrick Hell <78173981+Patrickhell@users.noreply.github.com>
2023-10-05 14:07:55 +0200

    Initialize attempt-4

---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,14 @@
+@section('title', 'Dashboard')
+
+<x-app-layout>
+    <div class="bd_dashboard-wrapper">
+        <div class="bd_dashboard-stats-wrapper w-full">
+            <x-dashboard.stats-cards :reviews="$reviews" :averageVote="$averageVote" :messages="$messages" :totalMessagesCount="$totalMessagesCount" :totalReviewsCount="$totalReviewsCount"/>
+        </div>
+            {{-- Message Container --}}
+            <x-dashboard.message-container :messages="$messages"/>
+
+            {{-- Reviews Container --}}
+            <x-dashboard.reviews-container :reviews="$reviews"/>
+    </div>
+</x-app-layout>
