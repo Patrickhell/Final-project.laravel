1a6b509936958260b80ec3b1b59ba63daab032ff
Patrick Hell <78173981+Patrickhell@users.noreply.github.com>
2023-10-05 14:07:58 +0200

    Initialize dev_validation

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>@yield('title', 'Default Title')</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+        <!-- Scripts -->
+        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased bg-cover bg-[url('https://img.freepik.com/free-photo/blank-letter-stationery-set_53876-147796.jpg?w=1380&t=st=1695230061~exp=1695230661~hmac=67849dc4dcf510615f84a13fd06c3ce8e3f71aea56c0bbb3ad96c7dbc051df61')]">
+        @include('layouts.navigation.aside')
+        @include('layouts.navigation.mobile')
+
+        <x-popup-create-profile />
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="{{ mix('/js/app.js') }}"></script>
+    </body>
+</html>
